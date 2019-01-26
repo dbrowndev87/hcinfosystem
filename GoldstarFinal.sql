47373711131d97542d3d6bd8fc7e3f013444da31
dbrown32 <dbrown32066@hollandcollege.com>
2019-01-25 21:30:10 -0400

    - Fixed SQL file, auto increment and trigger.

--- GoldstarFinal.sql
+++ GoldstarFinal.sql
@@ -3,17 +3,14 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 23, 2019 at 07:43 PM
+-- Generation Time: Jan 26, 2019 at 02:20 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.13
 
-SET SQL_MODE
-= "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT
-= 0;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone
-= "+00:00";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -28,17 +25,12 @@ DROP DATABASE `goldstar`;
 CREATE DATABASE `goldstar`;
 USE `goldstar`;
 
-
 DELIMITER $$
 --
 -- Procedures
 --
-CREATE DEFINER=`root`@`localhost` PROCEDURE `getAllStudents`
-(INOUT `type` INT
-(1))  NO SQL
-SELECT *
-FROM user
-WHERE user.type_code = type$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getAllStudents` (INOUT `type` INT(1))  NO SQL
+SELECT * FROM user WHERE user.type_code = type$$
 
 DELIMITER ;
 
@@ -48,16 +40,11 @@ DELIMITER ;
 -- Table structure for table `course`
 --
 
-CREATE TABLE `course`
-(
-  `course_id` varchar
-(12) NOT NULL,
-  `course_name` varchar
-(140) NOT NULL,
-  `dept_id` int
-(6) NOT NULL,
-  `credits` int
-(2) NOT NULL
+CREATE TABLE `course` (
+  `course_id` varchar(12) NOT NULL,
+  `course_name` varchar(140) NOT NULL,
+  `dept_id` int(6) NOT NULL,
+  `credits` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -66,23 +53,17 @@ CREATE TABLE `course`
 -- Table structure for table `department`
 --
 
-CREATE TABLE `department`
-(
-  `dept_id` int
-(6) NOT NULL,
-  `dept_name` varchar
-(140) NOT NULL
+CREATE TABLE `department` (
+  `dept_id` int(6) NOT NULL,
+  `dept_name` varchar(140) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `department`
 --
 
-INSERT INTO `department` (`
-dept_id`,
-`dept_name
-`) VALUES
-(1, 'Administrator'),
+INSERT INTO `department` (`dept_id`, `dept_name`) VALUES
+(1, 'Administration'),
 (2, 'Computer Information and Systems'),
 (3, 'Computer Networking');
 
@@ -92,18 +73,12 @@ dept_id`,
 -- Table structure for table `enrollment`
 --
 
-CREATE TABLE `enrollment`
-(
-  `enrollment_id` int
-(6) NOT NULL,
-  `student_id` int
-(6) NOT NULL,
-  `section_id` int
-(6) NOT NULL,
-  `course_status` varchar
-(20) NOT NULL,
-  `grade` double
-(5,2) NOT NULL
+CREATE TABLE `enrollment` (
+  `enrollment_id` int(6) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `student_id` int(6) NOT NULL,
+  `section_id` int(6) NOT NULL,
+  `course_status` varchar(20) NOT NULL,
+  `grade` double(5,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -112,14 +87,10 @@ CREATE TABLE `enrollment`
 -- Table structure for table `faculty`
 --
 
-CREATE TABLE `faculty`
-(
-  `faculty_id` int
-(6) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `faculty_status` varchar
-(20) NOT NULL,
-  `user_id` int
-(6) NOT NULL
+CREATE TABLE `faculty` (
+  `faculty_id` int(6) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `faculty_status` varchar(20) NOT NULL,
+  `user_id` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -128,22 +99,15 @@ CREATE TABLE `faculty`
 -- Table structure for table `section`
 --
 
-CREATE TABLE `section`
-(
-  `section_id` int
-(6) NOT NULL,
-  `faculty_id` int
-(6) NOT NULL,
+CREATE TABLE `section` (
+  `section_id` int(6) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `faculty_id` int(6) NOT NULL, 
   `start_date` date NOT NULL,
   `end_date` date NOT NULL,
-  `designation` char
-(1) NOT NULL,
-  `semester` varchar
-(10) NOT NULL,
-  `vacancy` int
-(3) NOT NULL,
-  `course_id` varchar
-(12) NOT NULL
+  `designation` char(1) NOT NULL,
+  `semester` varchar(10) NOT NULL,
+  `vacancy` int(3) NOT NULL,
+  `course_id` varchar(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -152,45 +116,34 @@ CREATE TABLE `section`
 -- Table structure for table `student`
 --
 
-CREATE TABLE `student`
-(
-  `student_id` int
-(6) NOT NULL,
-  `student_status` varchar
-(20) NOT NULL,
-  `gpa` double
-(5,2) NOT NULL,
-  `user_id` int
-(6) NOT NULL,
-  `amount_owing` double
-(8,2) NOT NULL
+CREATE TABLE `student` (
+  `student_id` int(6) NOT NULL,
+  `student_status` varchar(20) NOT NULL,
+  `gpa` double(5,2) NOT NULL,
+  `user_id` int(6) NOT NULL,
+  `amount_owing` double(8,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `transaction`
 --
 
-CREATE TABLE `transaction`
-(
-  `trans_id` int
-(10) NOT NULL,
-  `trans_amount` double
-(8,2) NOT NULL,
+CREATE TABLE `transaction` (
+  `trans_id` int(10) NOT NULL,
+  `trans_amount` double(8,2) NOT NULL,
   `trans_date` date NOT NULL,
-  `student_id` int
-(6) NOT NULL
+  `student_id` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Triggers `transaction`
 --
 DELIMITER $$
-CREATE TRIGGER `update_amount_owing` AFTER
-INSERT ON `transaction` FOR
-EACH
-ROW
-UPDATE student
+CREATE TRIGGER `update_amount_owing` AFTER INSERT ON `transaction` FOR EACH ROW UPDATE student
 SET student.amount_owing = student.amount_owing - new.trans_amount
+WHERE student.student_id = new.student_id
 $$
 DELIMITER ;
 
@@ -200,35 +153,27 @@ DELIMITER ;
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user`
-(
-  `user_id` int
-(6) NOT NULL,
-  `first_name` varchar
-(30) NOT NULL,
-  `last_name` varchar
-(30) NOT NULL,
-  `email` varchar
-(50) NOT NULL,
+CREATE TABLE `user` (
+  `user_id` int(6) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `birth_date` date NOT NULL,
-  `type_code` int
-(1) NOT NULL,
-  `address` varchar
-(140) NOT NULL,
-  `dept_id` int
-(6) NOT NULL
+  `type_code` int(1) NOT NULL,
+  `address` varchar(140) NOT NULL,
+  `dept_id` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user`
-  (`user_id`, `first_name
-  `, `last_name`, `email`, `birth_date`, `type_code`, `address`, `dept_id`) VALUES
-(1, 'Darcy', 'Brown', 'dbrown32066@hollandcollege.com', '2019-03-18', 1, '8 Bonnie Blink Dr.', 1),
-(2, 'darcy2', 'brown', 'asdasdasd', '2019-01-03', 2, '8 bonnie blink dr.', 1),
-(3, 'nicholas', 'peconi', 'asdasd', '2019-01-11', 1, 'asdasd', 1);
+INSERT INTO `user` (`user_id`, `first_name`, `last_name`, `email`, `birth_date`, `type_code`, `address`, `dept_id`) VALUES
+(1, 'Darcy', 'Brown', 'dbrown32066@hollandcollege.com', '2019-03-18', 1, '8 Bonnie Blink dr.', 1),
+(2, 'Test', 'Student', 'teststudent@happycollege.com', '2019-03-18', 3, '123 Student Lane.', 2),
+(3, 'Test', 'Faculty', 'testfaculty@happycollege.com', '2019-03-18', 2, '145 Faculty Lane.', 3),
+(4, 'Nicholas', 'Peconi', 'npeconi@hollandcollege.com', '2019-03-18', 1, '123 Peconi Lane.', 1),
+(5, 'Kristen', 'Murchison', 'kmurchison@hollandcollege.com', '2019-03-18', 1, '876 Murchison Lane.', 1);
 
 -- --------------------------------------------------------
 
@@ -236,29 +181,21 @@ INSERT INTO `user`
 -- Table structure for table `user_login`
 --
 
-CREATE TABLE `user_login`
-(
-  `username` varchar
-(12) NOT NULL,
-  `password` varchar
-(50) NOT NULL,
-  `active` tinyint
-(1) NOT NULL DEFAULT '0',
-  `user_id` int
-(6) NOT NULL
+CREATE TABLE `user_login` (
+  `username` varchar(12) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '0',
+  `user_id` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user_login`
 --
 
-INSERT INTO `user_login` (`
-username`,
-`password
-`, `active`, `user_id`) VALUES
+INSERT INTO `user_login` (`username`, `password`, `active`, `user_id`) VALUES
 ('dbrown32066', '5f4dcc3b5aa765d61d8327deb882cf99', 1, 1),
-('dbrown8218', 'e30bd39aa5e4a2c40efffd16b189dd60', 1, 2),
-('npeconi143', '5f4dcc3b5aa765d61d8327deb882cf99', 1, 3);
+('kmerchison', '5f4dcc3b5aa765d61d8327deb882cf99', 1, 5),
+('npeconi', '5f4dcc3b5aa765d61d8327deb882cf99', 1, 4);
 
 -- --------------------------------------------------------
 
@@ -266,22 +203,16 @@ username`,
 -- Table structure for table `user_type_code`
 --
 
-CREATE TABLE `user_type_code`
-(
-  `type_code` int
-(1) NOT NULL,
-  `user_description` varchar
-(20) NOT NULL
+CREATE TABLE `user_type_code` (
+  `type_code` int(1) NOT NULL,
+  `user_description` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user_type_code`
 --
 
-INSERT INTO `user_type_code` (`
-type_code`,
-`user_description
-`) VALUES
+INSERT INTO `user_type_code` (`type_code`, `user_description`) VALUES
 (1, 'Admin'),
 (2, 'Faculty'),
 (3, 'Student');
@@ -294,91 +225,78 @@ type_code`,
 -- Indexes for table `course`
 --
 ALTER TABLE `course`
-ADD PRIMARY KEY
-(`course_id`),
-ADD KEY `dept_id`
-(`dept_id`);
+  ADD PRIMARY KEY (`course_id`),
+  ADD KEY `dept_id` (`dept_id`);
 
 --
 -- Indexes for table `department`
 --
 ALTER TABLE `department`
-ADD PRIMARY KEY
-(`dept_id`);
+  ADD PRIMARY KEY (`dept_id`);
 
 --
 -- Indexes for table `enrollment`
 --
 ALTER TABLE `enrollment`
-ADD PRIMARY KEY
-(`enrollment_id`),
-ADD KEY `student_id`
-(`student_id`),
-ADD KEY `section_id`
-(`section_id`);
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `section_id` (`section_id`);
 
 --
 -- Indexes for table `faculty`
 --
 ALTER TABLE `faculty`
-ADD KEY `user_id`
-(`user_id`);
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `section`
 --
 ALTER TABLE `section`
-ADD PRIMARY KEY
-(`section_id`),
-ADD KEY `faculty_id`
-(`faculty_id`),
-ADD KEY `course_id`
-(`course_id`);
+  ADD KEY `faculty_id` (`faculty_id`),
+  ADD KEY `course_id` (`course_id`);
 
 --
 -- Indexes for table `student`
 --
 ALTER TABLE `student`
-ADD PRIMARY KEY
-(`student_id`),
-ADD KEY `user_id`
-(`user_id`);
+  ADD PRIMARY KEY (`student_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `transaction`
 --
 ALTER TABLE `transaction`
-ADD PRIMARY KEY
-(`trans_id`),
-ADD KEY `student_id`
-(`student_id`);
+  ADD PRIMARY KEY (`trans_id`),
+  ADD KEY `student_id` (`student_id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-ADD PRIMARY KEY
-(`user_id`),
-ADD KEY `type_code`
-(`type_code`),
-ADD KEY `dept_id`
-(`dept_id`);
+  ADD KEY `type_code` (`type_code`),
+  ADD KEY `dept_id` (`dept_id`);
 
 --
 -- Indexes for table `user_login`
 --
 ALTER TABLE `user_login`
-ADD PRIMARY KEY
-(`username`),
-ADD KEY `user_id`
-(`user_id`);
+  ADD PRIMARY KEY (`username`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `user_type_code`
 --
 ALTER TABLE `user_type_code`
-ADD PRIMARY KEY
-(`type_code`);
+  ADD PRIMARY KEY (`type_code`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `enrollment`
+--
+ALTER TABLE `enrollment`
+  MODIFY `enrollment_id` int(6) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -388,63 +306,51 @@ ADD PRIMARY KEY
 -- Constraints for table `course`
 --
 ALTER TABLE `course`
-ADD CONSTRAINT `course_ibfk_1` FOREIGN KEY
-(`dept_id`) REFERENCES `department`
-(`dept_id`);
+  ADD CONSTRAINT `course_ibfk_1` FOREIGN KEY (`dept_id`) REFERENCES `department` (`dept_id`);
+
+--
+-- Constraints for table `department`
+--
+ALTER TABLE `department`
+  ADD CONSTRAINT `department_ibfk_1` FOREIGN KEY (`dept_id`) REFERENCES `user` (`user_id`);
 
 --
 -- Constraints for table `enrollment`
 --
 ALTER TABLE `enrollment`
-ADD CONSTRAINT `enrollment_ibfk_1` FOREIGN KEY
-(`student_id`) REFERENCES `student`
-(`student_id`),
-ADD CONSTRAINT `enrollment_ibfk_2` FOREIGN KEY
-(`section_id`) REFERENCES `section`
-(`section_id`);
+  ADD CONSTRAINT `enrollment_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`),
+  ADD CONSTRAINT `enrollment_ibfk_2` FOREIGN KEY (`section_id`) REFERENCES `section` (`section_id`);
 
 --
 -- Constraints for table `faculty`
 --
 ALTER TABLE `faculty`
-ADD CONSTRAINT `faculty_ibfk_1` FOREIGN KEY
-(`user_id`) REFERENCES `user`
-(`user_id`);
+  ADD CONSTRAINT `faculty_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
 --
 -- Constraints for table `section`
 --
 ALTER TABLE `section`
-ADD CONSTRAINT `section_ibfk_1` FOREIGN KEY
-(`faculty_id`) REFERENCES `faculty`
-(`faculty_id`),
-ADD CONSTRAINT `section_ibfk_2` FOREIGN KEY
-(`course_id`) REFERENCES `course`
-(`course_id`);
+  ADD CONSTRAINT `section_ibfk_1` FOREIGN KEY (`faculty_id`) REFERENCES `faculty` (`faculty_id`),
+  ADD CONSTRAINT `section_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`);
 
 --
 -- Constraints for table `student`
 --
 ALTER TABLE `student`
-ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY
-(`user_id`) REFERENCES `user`
-(`user_id`);
+  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
 --
 -- Constraints for table `transaction`
 --
 ALTER TABLE `transaction`
-ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY
-(`student_id`) REFERENCES `student`
-(`student_id`);
+  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`);
 
 --
 -- Constraints for table `user`
 --
 ALTER TABLE `user`
-ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY
-(`type_code`) REFERENCES `user_type_code`
-(`type_code`);
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`type_code`) REFERENCES `user_type_code` (`type_code`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
