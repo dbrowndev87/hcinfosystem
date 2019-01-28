66327737ac89c7e0182821ac4054358acbb7b690
HC\kmurchison <kristen.murchison@gmail.com>
2019-01-28 16:42:40 -0400

    Styled login page and menu Changed home to show login screen Thought about playing with 3D js but not today

--- GoldStarApiFront/src/app/app-routing.module.ts
+++ GoldStarApiFront/src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './error-pages/not-found/not-found.component';
 import { InternalServerComponent } from './error-pages/internal-server/internal-server.component';
+import { LoginComponent } from './login/login-component/login-component.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: LoginComponent },
   { path: '404', component: NotFoundComponent },
   { path: '500', component: InternalServerComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
